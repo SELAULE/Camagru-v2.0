587bde9ee20c53c90cbc5b4eab64163c85bfdfca
Ntsako SELAULE <nselaule@e5r5p10.wethinkcode.co.za>
2019-04-15 15:17:59 +0200

    still upload

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ const onError  = (req, res) => {
 }
 
 const upload = multer({
-	dest: '/uploads/'
+	dest: './uploads/'
 })
 // Home page
 router.get('/', (req, res) => {
@@ -32,8 +32,17 @@ router.get('/cam', ensureAuthinticated, (req, res) =>
 	})
 );
 
-router.post('/cam', (req, res, next) => {
-
+router.post('/cam', upload.single('img64'), (req, res) => {
+	if (req.file) {
+		console.log('File is uploading... ');
+		var filename = req.file.filename;
+		var uploadStatus = 'File successfully Uploaded';
+	} else {
+		console.log('No File Uploaded');
+        var filename = 'FILE NOT UPLOADED';
+        var uploadStatus = 'File Upload Failed';
+	}
+	res.render('cam', {status: uploadStatus, filename: `The file is  + ${filename}` });
 });
 
 module.exports = router;
